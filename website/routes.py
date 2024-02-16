ce0f2c2323a79c60fe0362b962b0a5176e250c03
Nikitha2404 <hebbar.nikitha@gmail.com>
2024-02-16 23:43:07 +0530

    added route for home page

--- website/routes.py
+++ website/routes.py
@@ -1,6 +1,17 @@
-# from flask import render_template,url_for,redirect,request,send_file,jsonify
+from flask import render_template,url_for,redirect,request,send_file,jsonify
 from website import app
+import psycopg2
+
+def db_conn():
+    conn = psycopg2.connect(database="mydatabase", host="localhost", user="myuser",password="mypassword",port="5432")
+    return conn
 
 @app.route('/')
 def home():
-    return 'hello'
+    conn=db_conn()
+    cur=conn.cursor()
+    select_query = f'''SELECT stockname,tickers FROM stocks;'''
+    cur.execute(select_query)
+    data = cur.fetchall()
+
+    return render_template('home.html',data=data)
